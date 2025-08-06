92d1efe6fdadf210a43a14077734304a5b62e5d9
Anushka599 <anushka.bhakare@cumminscollege.in>
2025-08-06 22:25:22 +0530

    Code for Basic Calculator

--- calculator.py
+++ calculator.py
@@ -1,1 +1,31 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        n=len(s)
+        stack=[]
+        res=0
+        num=0
+        sign=1   
 
+        for i in s:
+            if i.isdigit():
+                num=num*10+int(i)
+            elif i=='+':
+                res+=sign*num
+                num=0
+                sign=1
+            elif i=='-':
+                res+=sign*num
+                num=0
+                sign=-1
+            elif i=='(':
+                stack.append(res)
+                stack.append(sign)
+                res=0
+                sign=1
+            elif i==')':
+                res+=sign*num
+                num=0
+                res*=stack.pop()
+                res+=stack.pop()
+        
+        return res+num*sign
